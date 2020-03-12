3b8837618b9fbb217e42d18c482b0fefa66b3aa6
Hongli Lai <h.lai@fullstaq.com>
2020-03-12 17:22:05 +0100

    OS detection support: specify friendly display names

--- src/main/java/io/jenkins/plugins/venafinextgencodesigning/JarSignerStepExecution.java
+++ src/main/java/io/jenkins/plugins/venafinextgencodesigning/JarSignerStepExecution.java
@@ -215,12 +215,23 @@ public class JarSignerStepExecution extends AbstractStepExecutionImpl {
     }
 
     public static enum OsType {
-        MACOS,
-        GENERIC_UNIX,
-        WINDOWS;
+        MACOS("macOS"),
+        GENERIC_UNIX("Unix (generic)"),
+        WINDOWS("Windows");
+
+        private String displayName;
+
+        private OsType(String displayName) {
+            this.displayName = displayName;
+        }
 
         public boolean isUnixCompatible() {
             return this == MACOS || this == GENERIC_UNIX;
+        }
+
+        @Override
+        public String toString() {
+            return displayName;
         }
     }
 
