0a8d78431dbcf7c3f933937e391b9bfec88ca4d0
Hongli Lai <h.lai@fullstaq.com>
2020-04-08 11:26:28 +0200

    signtool support: support signature verification
    Closes #37.

---
+++ src/main/java/io/jenkins/plugins/venafinextgencodesigning/SignToolVerifyBuilder.java
@@ -0,0 +1,164 @@
+package io.jenkins.plugins.venafinextgencodesigning;
+
+import hudson.Launcher;
+import hudson.Proc;
+import hudson.AbortException;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.AbstractProject;
+import hudson.model.Computer;
+import hudson.model.Node;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.Builder;
+import hudson.util.FormValidation;
+import hudson.tasks.BuildStepDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import jenkins.tasks.SimpleBuildStep;
+
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+public class SignToolVerifyBuilder extends Builder implements SimpleBuildStep {
+    @SuppressFBWarnings("UUF_UNUSED_FIELD")
+    private String fileOrGlob;
+
+    @SuppressFBWarnings("UUF_UNUSED_FIELD")
+    private String signToolInstallDir;
+
+    @DataBoundConstructor
+    public SignToolVerifyBuilder() {
+    }
+
+    public String getFileOrGlob() {
+        return fileOrGlob;
+    }
+
+    @DataBoundSetter
+    public void setFileOrGlob(String value) {
+        this.fileOrGlob = value;
+    }
+
+    public String getSignToolInstallDir() {
+        return signToolInstallDir;
+    }
+
+    @DataBoundSetter
+    public void setSignToolInstallDir(String value) {
+        this.signToolInstallDir = value;
+    }
+
+    @Override
+    public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener)
+        throws InterruptedException, IOException
+    {
+        Logger logger = new Logger(listener.getLogger(), Messages.JarSignerBuilder_functionName());
+        Computer wsComputer = workspace.toComputer();
+        if (wsComputer == null) {
+            throw new AbortException("Unable to retrieve computer for workspace");
+        }
+        Node wsNode = wsComputer.getNode();
+        if (wsNode == null) {
+            throw new AbortException("Unable to retrieve node for workspace");
+        }
+        FilePath nodeRoot = wsNode.getRootPath();
+        if (nodeRoot == null) {
+            throw new AbortException("Unable to retrieve root path of node");
+        }
+
+        AgentInfo agentInfo = nodeRoot.act(new AgentInfo.GetAgentInfo());
+        FilePath signToolPath = getSignToolPath(agentInfo, nodeRoot);
+
+        invokeCommand(logger, launcher, workspace,
+            "Verifying with signtool: " + getFileOrGlob() + "",
+            "Successfully verifying '" + getFileOrGlob() + "'.",
+            "Error verifying '" + getFileOrGlob() + "'",
+            "signtool",
+            true,
+            new String[]{
+                signToolPath.getRemote(),
+                "verify",
+                getFileOrGlob(),
+            },
+            null);
+    }
+
+    private String invokeCommand(Logger logger, Launcher launcher, FilePath ws,
+        String preMessage, String successMessage, String errorMessage,
+        String shortCommandLine,  boolean printOutputOnSuccess, String[] cmdArgs,
+        boolean[] masks)
+        throws InterruptedException, IOException
+    {
+        logger.log("%s", preMessage);
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Launcher.ProcStarter starter =
+            launcher.
+            launch().
+            cmds(cmdArgs).
+            stdout(output).
+            pwd(ws);
+        if (masks != null) {
+            starter.masks(masks);
+        }
+
+        Proc proc;
+        int code;
+        try {
+            proc = starter.start();
+            code = proc.join();
+        } catch (IOException e) {
+            logger.log("%s: %s", errorMessage, e.getMessage());
+            throw e;
+        }
+
+        String outputString = output.toString("UTF-8").trim();
+        if (code == 0) {
+            if (printOutputOnSuccess) {
+                logger.log("%s", outputString);
+            }
+            logger.log("%s", successMessage);
+            return outputString;
+        } else {
+            logger.log(
+                "%s: command exited with code %d. Output from command '%s' is as follows:\n%s",
+                errorMessage, code, shortCommandLine, outputString);
+            throw new AbortException(errorMessage + ": command exited with code " + code);
+        }
+    }
+
+    private FilePath getSignToolPath(AgentInfo agentInfo, FilePath nodeRoot) {
+        String arch = agentInfo.isWindows64Bit ? "x64" : "x86";
+        return nodeRoot.child(getSignToolInstallDir()).child(arch).child("signtool.exe");
+    }
+
+    @Symbol("venafiVerifyWithSignTool")
+    @Extension
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        @SuppressWarnings("rawtypes")
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return Messages.SignToolVerifyBuilder_displayName();
+        }
+
+        public FormValidation doCheckFileOrGlob(@QueryParameter String value) {
+            return FormValidation.validateRequired(value);
+        }
+
+        public FormValidation doCheckSignToolInstallDir(@QueryParameter String value) {
+            return FormValidation.validateRequired(value);
+        }
+    }
+}
